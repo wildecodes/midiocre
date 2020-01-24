deadc765d9b8e2d3f2492297de1ca942fde7ae95
wildecodes <34753004+wildecodes@users.noreply.github.com>
2020-01-24 09:50:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const sampler = new Tone.Players({
+  'kick': 'https://cdn.jsdelivr.net/gh/Tonejs/Tone.js/examples/audio/505/kick.mp3',
+  'snare': 'https://cdn.jsdelivr.net/gh/Tonejs/Tone.js/examples/audio/505/snare.mp3',
+  'kick': 'https://cdn.jsdelivr.net/gh/Tonejs/Tone.js/examples/audio/505/kick.mp3',
+})
+
+const volume = new Tone.Volume(-12).toMaster();
+
+sampler.connect(volume);
+
+const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+const song = [
+ kick: [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0],
+ hihat: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+ snare: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+]
+
+
+
+
+function start() {
+  Tone.Transport.bmp.value = 130;
+
+  const sequence = new Tone.Sequence(function (time, idx) {
+    sampler.get('kick').start();
+  } [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], "8n")
+
+  Tone.Transport.start();
+  sequence.start();
+}
